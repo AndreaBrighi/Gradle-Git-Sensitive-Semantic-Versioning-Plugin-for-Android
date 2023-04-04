4a1caa3b4e7b71d1ac3a6184bb7fa517da5e2415
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-04-05 00:34:45 +0200

    test: add tests for plugin

---
+++ src/test/kotlin/Tests.kt
@@ -0,0 +1,318 @@
+// import io.kotest.assertions.throwables.shouldThrowUnit
+import io.kotest.core.spec.style.StringSpec
+// import io.kotest.matchers.shouldBe
+// import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.string.shouldContain
+// import io.kotest.matchers.string.shouldNotContain
+import org.gradle.internal.impldep.org.junit.rules.TemporaryFolder
+import org.gradle.testkit.runner.GradleRunner
+// import org.gradle.testkit.runner.UnexpectedBuildFailure
+import java.util.concurrent.TimeUnit
+
+internal class Tests : StringSpec(
+    {
+        "minimal configuration increment version code" {
+            val project = configuredPlugin()
+            val result = project.runGradle()
+            println(result)
+            result shouldContain "0.1.0-archeo+"
+            val resultCode = project.runGradleCode()
+            println(resultCode)
+            resultCode shouldContain "0"
+        }
+
+        "minimal configuration semantic version code" {
+            val project = configuredPlugin("incrementalCode.set(false)")
+            val result = project.runGradle()
+            println(result)
+            result shouldContain "0.1.0-archeo+"
+            val resultCode = project.runGradleCode()
+            println(resultCode)
+            resultCode shouldContain "1000"
+        }
+
+        "simple usage of extension increment version code" {
+            val project = configuredPlugin("noTagIdentifier.set(\"foo\")")
+            val result = project.runGradle()
+            println(result)
+            result shouldContain "0.1.0-foo+"
+            val resultCode = project.runGradleCode()
+            println(resultCode)
+            resultCode shouldContain "0"
+        }
+
+        "simple usage of extension semantic version code" {
+            val project = configuredPlugin(
+                """
+                noTagIdentifier.set("foo")
+                preReleaseSeparator.set("-")
+                incrementalCode.set(false)
+                """.trimIndent(),
+            )
+            val result = project.runGradle()
+            println(result)
+            result shouldContain "0.1.0-foo+"
+            val resultCode = project.runGradleCode()
+            println(resultCode)
+            resultCode shouldContain "1000"
+        }
+
+        "git single commit semantic version code" {
+            val project = configuredPlugin(
+                """
+                noTagIdentifier.set("foo")
+                incrementalCode.set(false)
+                """.trimIndent(),
+            ) {
+                initGit()
+            }
+            val result = project.runGradle()
+            println(result)
+            result shouldContain "0.1.0-foo"
+            val resultCode = project.runGradleCode()
+            println(resultCode)
+            resultCode shouldContain "1000"
+        }
+
+        "git single commit increment version code" {
+            val project = configuredPlugin("noTagIdentifier.set(\"foo\")") {
+                initGit()
+            }
+            val result = project.runGradle()
+            println(result)
+            result shouldContain "0.1.0-foo"
+            val resultCode = project.runGradleCode()
+            println(resultCode)
+            resultCode shouldContain "1"
+        }
+
+        "git multiple commits semantic version code" {
+            val project = configuredPlugin(
+                """
+                noTagIdentifier.set("foo")
+                """.trimIndent(),
+            ) {
+                initGit()
+                file("something") { "something" }
+                runCommand("git add something")
+                runCommand("git", "commit", "-m", "\"Test commit 2\"")
+            }
+            val result = project.runGradle()
+            println(result)
+            result shouldContain "0.1.0-foo"
+            val resultCode = project.runGradleCode()
+            println(resultCode)
+            resultCode shouldContain "2"
+        }
+
+        /*"git tagged commit" {
+            val project = configuredPlugin("noTagIdentifier.set(\"foo\")") {
+                initGitWithTag()
+            }
+            val result = project.runGradle()
+            println(result)
+            result.lines().any { it matches Regex(""".*1\.2\.3$""") } shouldBe true
+            val resultCode = project.runGradleCode()
+            println(resultCode)
+            resultCode shouldContain "1"
+        }
+        "git 2 tagged commit" {
+            val workingDirectory = configuredPlugin("developmentIdentifier.set(\"foodev\")") {
+                initGitWithTag()
+                file("something") { "something" }
+                runCommand("git add something")
+                runCommand("git", "commit", "-m", "\"Test commit 2\"")
+                runCommand("git", "tag", "-a", "1.2.4", "-m", "\"test\"")
+            }
+            val result = workingDirectory.runGradle()
+            println(result)
+            result.lines().any { it matches Regex(""".*1\.2\.4$""") } shouldBe true
+            val resultCode = workingDirectory.runGradleCode()
+            println(resultCode)
+            resultCode shouldContain "2"
+        }
+        "git tagged commit overflow" {
+            val project = configuredPlugin(
+                """
+                noTagIdentifier.set("foo")
+                versionCodePatchDigits.set(0)
+                """.trimIndent()
+            ) {
+                initGitWithTag()
+            }
+            val result = project.runGradle()
+            println(result)
+            result.lines().any { it matches Regex(""".*1\.2\.3$""") } shouldBe true
+            val resultCode = project.runGradleCode()
+            println(resultCode)
+            resultCode shouldContain "1002003"
+        }
+        "git tagged + development" {
+            val workingDirectory = configuredPlugin("developmentIdentifier.set(\"foodev\")") {
+                initGitWithTag()
+                file("something") { "something" }
+                runCommand("git add something")
+                runCommand("git", "commit", "-m", "\"Test commit 2\"")
+            }
+            val result = workingDirectory.runGradle()
+            println(result)
+            val expectedVersion = "1.2.3-foodev01+"
+            result shouldContain expectedVersion
+            with(workingDirectory) {
+                runCommand("git", "tag", "-a", "test", "-m", "test tag")
+                val newResult = runGradle()
+                println(newResult)
+                newResult shouldContain expectedVersion
+            }
+            with(workingDirectory) {
+                runCommand("git", "tag", "-a", "1.2.4", "-m", "\"test\"")
+                val newResult = runGradle()
+                println(newResult)
+                newResult.lineSequence().find { it.matches(".*1.2.4$".toRegex()) } shouldNotBe null
+            }
+        }
+        "manual assignment of version" {
+            val workingDirectory = configuredPlugin("assignGitSemanticVersion()")
+            val result = workingDirectory.runGradle()
+            println(result)
+            val expectedVersion = "0.1.0-archeo"
+            result shouldContain expectedVersion
+        }
+        "regression for bug #2: https://github.com/DanySK/git-sensitive-semantic-versioning-gradle-plugin/issues/2" {
+            val workingDirectory = configuredPlugin(
+                """
+                minimumVersion.set("0.1.0")
+                developmentIdentifier.set("")    // <--- NOTICE THIS
+                noTagIdentifier.set("")          // <--- NOTICE THIS
+                developmentCounterLength.set(2)
+                """.trimIndent()
+            )
+            val result = workingDirectory.runGradle()
+            val expectedVersion = "0.1.0+"
+            result shouldContain expectedVersion
+        }
+        "support for lightweight tags (#323)" {
+            val workingDirectory = configuredPlugin()
+            with(workingDirectory) {
+                initGit()
+                runCommand("git", "tag", "1.2.3")
+            }
+            workingDirectory.runGradle() shouldContain "1.2.3"
+        }
+        "exclusion of lightweight tags (#323)" {
+            val workingDirectory = configuredPlugin(
+                """
+                excludeLightweightTags()
+                """.trimIndent()
+            )
+            with(workingDirectory) {
+                initGit()
+                runCommand("git", "tag", "1.2.3")
+            }
+            workingDirectory.runGradle() shouldNotContain "1.2.3"
+        }
+        "force the version" {
+            val result = configuredPlugin().runGradle("-PforceVersion=1.2.3", "printGitSemVer", "--stacktrace")
+            print(result)
+            result shouldContain "1.2.3"
+        }
+        "force the version with a non compliant version" {
+            shouldThrowUnit<UnexpectedBuildFailure> {
+                configuredPlugin().runGradle("-PforceVersion=a.b.c", "printGitSemVer", "--stacktrace")
+            }
+        }
+        "force the version with a custom property" {
+            val result = configuredPlugin(
+                """
+                forceVersionPropertyName.set("customVersion")
+                """.trimIndent()
+            ).runGradle("-PcustomVersion=1.2.3", "printGitSemVer", "--stacktrace")
+            print(result)
+            result shouldContain "1.2.3"
+        }*/
+    },
+) {
+    companion object {
+
+        fun folder(closure: TemporaryFolder.() -> Unit) = TemporaryFolder().apply {
+            create()
+            closure()
+        }
+
+        fun TemporaryFolder.file(name: String, content: () -> String) = newFile(name).writeText(content().trimIndent())
+
+        fun TemporaryFolder.runCommand(vararg command: String, wait: Long = 10) {
+            val process = ProcessBuilder(*command)
+                .directory(root)
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
+                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                .start()
+            process.waitFor(wait, TimeUnit.SECONDS)
+            require(process.exitValue() == 0) {
+                "command '${command.joinToString(" ")}' failed with exit value ${process.exitValue()}"
+            }
+        }
+
+        fun TemporaryFolder.runCommand(command: String, wait: Long = 10) = runCommand(
+            *command.split(" ").toTypedArray(),
+            wait = wait,
+        )
+
+        fun TemporaryFolder.initGit() {
+            runCommand("git init")
+            runCommand("git add .")
+            runCommand("git config user.name gitsemver")
+            runCommand("git config user.email none@test.com")
+            runCommand("git config --global init.defaultBranch master")
+            runCommand("git", "commit", "-m", "\"Test commit\"")
+        }
+
+        fun TemporaryFolder.initGitWithTag() {
+            initGit()
+            runCommand("git", "tag", "-a", "1.2.3", "-m", "\"test\"")
+        }
+
+        fun TemporaryFolder.moreTags() {
+            runCommand("git add .")
+            runCommand("git", "commit", "-m", "\"Test commit\"")
+            runCommand("git", "tag", "-a", "1.2.4", "-m", "\"test\"")
+        }
+
+        fun TemporaryFolder.runGradle(
+            vararg arguments: String = arrayOf("printGitSemVer", "--stacktrace"),
+        ): String = GradleRunner
+            .create()
+            .withProjectDir(root)
+            .withPluginClasspath()
+            .withArguments(*arguments)
+            .build().output
+
+        fun TemporaryFolder.runGradleCode(
+            vararg arguments: String = arrayOf("printGitSemVerCode", "--stacktrace"),
+        ): String = GradleRunner
+            .create()
+            .withProjectDir(root)
+            .withPluginClasspath()
+            .withArguments(*arguments)
+            .build().output
+
+        fun configuredPlugin(
+            pluginConfiguration: String = "",
+            otherChecks: TemporaryFolder.() -> Unit = {},
+        ): TemporaryFolder = folder {
+            file("settings.gradle") { "rootProject.name = 'testproject'" }
+            file("build.gradle.kts") {
+                """
+                plugins {
+                    id("org.brighiandrea.git-semver")
+                }
+                androidGitSemVer {
+                    $pluginConfiguration
+                }
+               
+                """.trimIndent()
+            }
+            otherChecks()
+        }
+    }
+}
