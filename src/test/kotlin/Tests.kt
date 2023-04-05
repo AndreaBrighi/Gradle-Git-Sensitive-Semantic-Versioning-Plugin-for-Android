e5286e26591e8a18e14519a06720982294554a60
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-04-05 19:36:01 +0200

    test: redactor tests

--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -1,12 +1,7 @@
-// import io.kotest.assertions.throwables.shouldThrowUnit
 import io.kotest.core.spec.style.StringSpec
-// import io.kotest.matchers.shouldBe
-// import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.string.shouldContain
-// import io.kotest.matchers.string.shouldNotContain
 import org.gradle.internal.impldep.org.junit.rules.TemporaryFolder
 import org.gradle.testkit.runner.GradleRunner
-// import org.gradle.testkit.runner.UnexpectedBuildFailure
 import java.util.concurrent.TimeUnit
 
 internal class Tests : StringSpec(
@@ -93,9 +88,7 @@ internal class Tests : StringSpec(
                 """.trimIndent(),
             ) {
                 initGit()
-                file("something") { "something" }
-                runCommand("git add something")
-                runCommand("git", "commit", "-m", "\"Test commit 2\"")
+                moreCommits()
             }
             val result = project.runGradle()
             println(result)
@@ -104,132 +97,6 @@ internal class Tests : StringSpec(
             println(resultCode)
             resultCode shouldContain "2"
         }
-
-        /*"git tagged commit" {
-            val project = configuredPlugin("noTagIdentifier.set(\"foo\")") {
-                initGitWithTag()
-            }
-            val result = project.runGradle()
-            println(result)
-            result.lines().any { it matches Regex(""".*1\.2\.3$""") } shouldBe true
-            val resultCode = project.runGradleCode()
-            println(resultCode)
-            resultCode shouldContain "1"
-        }
-        "git 2 tagged commit" {
-            val workingDirectory = configuredPlugin("developmentIdentifier.set(\"foodev\")") {
-                initGitWithTag()
-                file("something") { "something" }
-                runCommand("git add something")
-                runCommand("git", "commit", "-m", "\"Test commit 2\"")
-                runCommand("git", "tag", "-a", "1.2.4", "-m", "\"test\"")
-            }
-            val result = workingDirectory.runGradle()
-            println(result)
-            result.lines().any { it matches Regex(""".*1\.2\.4$""") } shouldBe true
-            val resultCode = workingDirectory.runGradleCode()
-            println(resultCode)
-            resultCode shouldContain "2"
-        }
-        "git tagged commit overflow" {
-            val project = configuredPlugin(
-                """
-                noTagIdentifier.set("foo")
-                versionCodePatchDigits.set(0)
-                """.trimIndent()
-            ) {
-                initGitWithTag()
-            }
-            val result = project.runGradle()
-            println(result)
-            result.lines().any { it matches Regex(""".*1\.2\.3$""") } shouldBe true
-            val resultCode = project.runGradleCode()
-            println(resultCode)
-            resultCode shouldContain "1002003"
-        }
-        "git tagged + development" {
-            val workingDirectory = configuredPlugin("developmentIdentifier.set(\"foodev\")") {
-                initGitWithTag()
-                file("something") { "something" }
-                runCommand("git add something")
-                runCommand("git", "commit", "-m", "\"Test commit 2\"")
-            }
-            val result = workingDirectory.runGradle()
-            println(result)
-            val expectedVersion = "1.2.3-foodev01+"
-            result shouldContain expectedVersion
-            with(workingDirectory) {
-                runCommand("git", "tag", "-a", "test", "-m", "test tag")
-                val newResult = runGradle()
-                println(newResult)
-                newResult shouldContain expectedVersion
-            }
-            with(workingDirectory) {
-                runCommand("git", "tag", "-a", "1.2.4", "-m", "\"test\"")
-                val newResult = runGradle()
-                println(newResult)
-                newResult.lineSequence().find { it.matches(".*1.2.4$".toRegex()) } shouldNotBe null
-            }
-        }
-        "manual assignment of version" {
-            val workingDirectory = configuredPlugin("assignGitSemanticVersion()")
-            val result = workingDirectory.runGradle()
-            println(result)
-            val expectedVersion = "0.1.0-archeo"
-            result shouldContain expectedVersion
-        }
-        "regression for bug #2: https://github.com/DanySK/git-sensitive-semantic-versioning-gradle-plugin/issues/2" {
-            val workingDirectory = configuredPlugin(
-                """
-                minimumVersion.set("0.1.0")
-                developmentIdentifier.set("")    // <--- NOTICE THIS
-                noTagIdentifier.set("")          // <--- NOTICE THIS
-                developmentCounterLength.set(2)
-                """.trimIndent()
-            )
-            val result = workingDirectory.runGradle()
-            val expectedVersion = "0.1.0+"
-            result shouldContain expectedVersion
-        }
-        "support for lightweight tags (#323)" {
-            val workingDirectory = configuredPlugin()
-            with(workingDirectory) {
-                initGit()
-                runCommand("git", "tag", "1.2.3")
-            }
-            workingDirectory.runGradle() shouldContain "1.2.3"
-        }
-        "exclusion of lightweight tags (#323)" {
-            val workingDirectory = configuredPlugin(
-                """
-                excludeLightweightTags()
-                """.trimIndent()
-            )
-            with(workingDirectory) {
-                initGit()
-                runCommand("git", "tag", "1.2.3")
-            }
-            workingDirectory.runGradle() shouldNotContain "1.2.3"
-        }
-        "force the version" {
-            val result = configuredPlugin().runGradle("-PforceVersion=1.2.3", "printGitSemVer", "--stacktrace")
-            print(result)
-            result shouldContain "1.2.3"
-        }
-        "force the version with a non compliant version" {
-            shouldThrowUnit<UnexpectedBuildFailure> {
-                configuredPlugin().runGradle("-PforceVersion=a.b.c", "printGitSemVer", "--stacktrace")
-            }
-        }
-        "force the version with a custom property" {
-            val result = configuredPlugin(
-                """
-                forceVersionPropertyName.set("customVersion")
-                """.trimIndent()
-            ).runGradle("-PcustomVersion=1.2.3", "printGitSemVer", "--stacktrace")
-            print(result)
-            result shouldContain "1.2.3"
-        }*/
     },
 ) {
     companion object {
@@ -276,6 +143,12 @@ internal class Tests : StringSpec(
             runCommand("git add .")
             runCommand("git", "commit", "-m", "\"Test commit\"")
             runCommand("git", "tag", "-a", "1.2.4", "-m", "\"test\"")
+        }
+
+        fun TemporaryFolder.moreCommits() {
+            file("something") { "something" }
+            runCommand("git add .")
+            runCommand("git", "commit", "-m", "\"Test commit\"")
         }
 
         fun TemporaryFolder.runGradle(
