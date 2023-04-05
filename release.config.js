b69de777623bb8289e304ea48cbf9dbd4cbb9ab3
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-04-05 17:56:31 +0200

    ci: minor fix in release.config.js

--- release.config.js
+++ release.config.js
@@ -1,17 +1,13 @@
 var publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 1
-git push --force origin \${nextRelease.version} || exit 3
+git push --force origin \${nextRelease.version} || exit 2
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
     ["@semantic-release/exec", {
         "publishCmd": publishCmd,
     }],
-    ["@semantic-release/github", {
-        "assets": [
-            { "path": "app/build/outputs/apk/release/*.apk" },
-        ]
-    }],
+    "@semantic-release/github",
     "@semantic-release/git",
 )
 config.branches = [ "main" ]
