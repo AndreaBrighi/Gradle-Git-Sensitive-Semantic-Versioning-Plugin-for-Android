ce83065aae1c14ee26a2b11a609be65dfc2869a1
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-11-13 23:28:08 +0100

    build: add conventional commit for next version computation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import io.github.andreabrighi.gradle.gitsemver.conventionalcommit.ConventionalCommit
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.config.KotlinCompilerVersion.VERSION as KOTLIN_VERSION
@@ -14,6 +15,18 @@ plugins {
     signing
     `maven-publish`
     alias(libs.plugins.taskTree)
+}
+
+gitSemVer {
+    maxVersionLength.set(20)
+    buildMetadataSeparator.set("-")
+    commitNameBasedUpdateStrategy(ConventionalCommit::semanticVersionUpdate)
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.convetional)
+    }
 }
 
 group = "io.github.andreabrighi"
@@ -33,11 +46,6 @@ class ProjectInfo {
 }
 
 val info = ProjectInfo()
-
-gitSemVer {
-    maxVersionLength.set(20)
-    buildMetadataSeparator.set("-")
-}
 
 repositories {
     mavenCentral()
