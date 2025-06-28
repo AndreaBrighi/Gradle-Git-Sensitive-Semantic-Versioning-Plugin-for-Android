9e236ef51470326949467152862bf8de6c289385
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2025-06-28 02:06:22 +0200

    build: update built

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
-    id("com.gradle.enterprise") version "3.19.2"
+    id("com.gradle.develocity") version "3.17"
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.26"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-gradleEnterprise {
+develocity {
     buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-        publishOnFailure()
+        termsOfUseUrl = "https://gradle.com/terms-of-use"
+        termsOfUseAgree = "yes"
+        publishing.onlyIf { it.buildResult.failures.isNotEmpty() }
     }
 }
 
