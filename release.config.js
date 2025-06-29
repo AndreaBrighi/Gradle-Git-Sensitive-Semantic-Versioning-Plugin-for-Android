f6d504c1bee9da719eb98f7341b0beeefcce8abb
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2025-06-29 03:58:02 +0200

    ci: change maven publishing

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 var publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 1
-./gradlew publish -PsigningKey="$SIGNING_KEY" -PsigningPassword="$SIGNING_PASSWORD" || exit 3
+./gradlew publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication -PsigningKey="$SIGNING_KEY" -PsigningPassword="$SIGNING_PASSWORD" || exit 3
 git push --force origin \${nextRelease.version} || exit 4
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
